e89eefcef2d31d0e1f81c5097151749720464c6e
pglauner <patrick.oliver.glauner@gmail.com>
2015-02-03 13:38:42 +0000

    Better covariance matrix script

--- week_1/exercise_5.py
+++ week_1/exercise_5.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 DATA_PATH = "data/square_data.csv"
 
@@ -7,3 +7,7 @@ DATA_PATH = "data/square_data.csv"
 if __name__ == '__main__':
     data = np.genfromtxt(DATA_PATH, delimiter=",")
     print np.cov(data.T, bias=1)
+
+    plt.scatter(data.T[0], data.T[1])
+    plt.grid()
+    plt.show()
