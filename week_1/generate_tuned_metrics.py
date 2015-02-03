b5cd5e880ef01ca5a4154b49d09b8c955e4b84a8
Francesco Ceccon <francesco@ceccon.me>
2015-02-03 12:38:49 +0000

    Update generate tuned metrics script to use new interface

--- week_1/generate_tuned_metrics.py
+++ week_1/generate_tuned_metrics.py
@@ -3,17 +3,14 @@ import time
 
 LOG_PATH = '/home/pi/Documents/log/'
 ANGLE = 30.0
-TOL = 0.05
 
 
 if __name__ == '__main__':
-    interface, motors = rocommon.setup()
+    robot = rocommon.Robot()
 
     interface.startLogging(LOG_PATH + 'tuned_data')
-    interface.increaseMotorAngleReferences(motors, [ANGLE, ANGLE])
 
-    while not interface.motorAngleReferencesReached(motors):
-        pass
+    robot._move_by_angle(ANGLE)
 
     # collect more data after reaching reference angle
     time.sleep(5.0)
