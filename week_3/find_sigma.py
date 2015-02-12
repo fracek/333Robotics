643b64ef55948355ec15964cd5f4bb995834ab8a
Francesco Ceccon <francesco@ceccon.me>
2015-02-12 10:37:50 +0000

    estimate g sigma

--- week_3/find_sigma.py
+++ week_3/find_sigma.py
@@ -3,16 +3,23 @@ sys.path.append('../lib')
 import rocommon
 
 STEP_DISTANCE = 20
+E_SIGMA = 0.5
 F_SIGMA = 0.1
+G_SIGMA = 0.5
 
 if __name__ == "__main__":
-    robot = rocommon.ProbabilisticRobot(e_sigma=0.5, f_sigma=F_SIGMA)
+    robot = rocommon.ProbabilisticRobot(e_sigma=E_SIGMA, f_sigma=F_SIGMA, g_sigma=G_SIGMA)
 
     try:
-        for dist in xrange(0, 200 / STEP_DISTANCE):
-            robot.move_forward(STEP_DISTANCE)
-            print('{} =  {}'.format(dist, robot.position_estimate()))
-            sys.stdin.readline()
+        if test_straight_movement:
+            for dist in xrange(0, 200 / STEP_DISTANCE):
+                robot.move_forward(STEP_DISTANCE)
+                print('{} =  {}'.format(dist, robot.position_estimate()))
+                sys.stdin.readline()
+        else:
+            for angle in xrange(0, 10):
+                robot.turn(2 * pi / 10.0)
+                print('{} =  {}'.format(dist, robot.position_estimate()))
     except KeyboardInterrupt:
         pass
     robot.implode()
