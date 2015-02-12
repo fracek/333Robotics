70a01250936ebd1107edb066872ddd3adbe4bf73
Francesco Ceccon <francesco@ceccon.me>
2015-02-12 09:51:50 +0000

    find sigma

---
+++ week_3/find_sigma.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('../lib')
+import rocommon
+
+SIGMA = 0.3
+STEP_DISTANCE = 20
+
+if __name__ == "__main__":
+    robot = rocommon.ProbabilisticRobot(sigma=SIGMA)
+
+    try:
+        for _ in xrange(0, 200 / STEP_DISTANCE):
+            robot.move_forward(STEP_DISTANCE)
+            print('I AM IN {}'.format(robot.position_estimate()))
+    except KeyboardInterrupt:
+        pass
+    robot.implode()
