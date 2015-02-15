cf5ad16c16a3421f07c204cfb2a468e77ef74eb6
Francesco Ceccon <francesco@ceccon.me>
2015-02-15 16:05:40 +0000

    More robust input parsing for w3/exercise_2

--- week_3/exercise_2.py
+++ week_3/exercise_2.py
@@ -1,15 +1,30 @@
+import ast
 import sys
 sys.path.append('../lib')
 import rocommon
+
+
+def parse_input(s):
+    try:
+        wp = ast.literal_eval(s)
+        if type(wp) == list and len(wp) == 2:
+            return wp
+    except SyntaxError:
+        pass
+    return None
+
 
 if __name__ == "__main__":
     robot = rocommon.ProbabilisticRobot()
 
     try:
         while True:
-            wp = input('Waypoint ([x, y]): ')
-            robot.move_to_waypoint(wp)
-            robot.motors['L']['instance'].angle()
-            print('{}'.format(robot.position_estimate()))
+            s = raw_input('Waypoint ([x, y]): ')
+            wp = parse_input(s)
+            if wp:
+                robot.move_to_waypoint(wp)
+                print('Current Position: {}'.format(robot.position_estimate()))
+            else:
+                print('Invalid input {}'.format(s))
     except KeyboardInterrupt:
         robot.implode()
