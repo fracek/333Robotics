e1eaabe5a7759c9dd9c67864dfb182661a039477
Francesco Ceccon <francesco@ceccon.me>
2015-02-17 19:10:49 +0000

    Check angle within sonar range

--- lib/rocommon/probabilistic_robot.py
+++ lib/rocommon/probabilistic_robot.py
@@ -44,19 +44,29 @@ class ProbabilisticRobot(Robot):
         [x, y, theta] = pos
         def compute_distance_from_wall(wall):
             (Ax, Ay, Bx, By) = wall
+
+            # Check angle beta between sonar and the normal to the wall
+            num = np.cos(theta)*(Ay - By) + np.sin(theta) * (Bx - Ax)
+            den = np.sqrt(np.square(Ay - By) + np.square(Bx - Ax))
+            beta = np.arccos(num / den)
+            if np.abs(beta) > np.pi/4.0:
+                return np.inf
+
+            # Compute distanec between sonar and wall
             num = (By - Ay) * (Ax - x) - (Bx - Ax) * (Ay - y)
             den = (By - Ay) * np.cos(theta) - (Bx - Ax) * np.sin(theta)
             m = num / den
-            if m <= 0:
-                return np.inf
-            # http://www.lucidarme.me/?p=1952
+
+            # Check hit point on the wall
+            # SEE: http://www.lucidarme.me/?p=1952
             C = np.array([x + m * np.cos(theta), y + m * np.sin(theta)])
             AB = [Bx - Ax, By - Ay]
             AC = C - [Ax, Ay]
             K_ac = np.dot(AB, AC)
             K_ab = np.dot(AB, AB)
-            if 0 < K_ac < K_ab:
+            if (0 < K_ac < K_ab) and m >= 0:
                 return m
+
             return np.inf
 
         distances = [compute_distance_from_wall(w) for w in self.map.walls]
