b9d22f758c7d03d858cc3740f578ab92bb8ac4d1
Francesco Ceccon <francesco@ceccon.me>
2015-02-18 11:27:50 +0000

    Move to waypoints

--- week_4/waypoint_navigation.py
+++ week_4/waypoint_navigation.py
@@ -3,6 +3,15 @@ sys.path.append('../lib')
 import rocommon
 
 STEP_DISTANCE = 10
+WAYPOINTS = [
+        [180, 30],
+        [180, 54],
+        [126, 54],
+        [126, 168],
+        [126, 126],
+        [30, 54],
+        [84, 54],
+        [84, 30]]
 
 if __name__ == "__main__":
     try:
@@ -10,7 +19,10 @@ if __name__ == "__main__":
         lab_map.draw()
         robot = rocommon.ProbabilisticRobot(
                 map=lab_map, starting_x=[84.0, 30.0, 0.0])
-        robot.update_measurement()
+        for wp in WAYPOINTS:
+            robot.move_to_waypoint(wp)
+            robot.update_measurement()
+            robot.draw_particles()
     except KeyboardInterrupt:
         pass
     robot.implode()
