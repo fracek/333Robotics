5d5c2423da914add81abf2d890abcbaea8752025
Francesco Ceccon <francesco@ceccon.me>
2015-03-05 10:09:14 +0000

    Place recognition

--- lib/rocommon/final_robot.py
+++ lib/rocommon/final_robot.py
@@ -11,21 +11,41 @@ class FinalRobot(ProbabilisticRobot):
         ProbabilisticRobot.__init__(self, use_spinning_sonar=True,
                                     starting_x=[0, 0, 0], map=map)
         self.num_bins = num_bins
+        self.num_sig = num_sig
         self.sig_location = sig_location
         self.signatures = np.zeros((num_sig, num_bins))
 
-    def register_location(self, idx):
+    def location_sig(self):
         sig = np.zeros(self.num_bins)
         for i in range(self.num_bins):
             self.sonar.rotate_by(2.0 * np.pi / float(self.num_bins))
             sonar_value = self.sonar.value()
             sig[i] = sonar_value
         self.sonar.rotate_by(-2.0 * np.pi)
+        return sig
+
+    def register_location(self, idx):
+        sig = self.location_sig()
         np.save(self._location_path(idx), sig)
 
     def load_location(self, idx):
         sig = np.load(self._location_path(idx))
         self.signatures[idx, :] = sig
 
+    def load_all_locations(self):
+        for i in range(0, self.num_sig):
+            self.load_location(i)
+
     def _location_path(self, idx):
-        return '{}/loc_{}'.format(self.sig_location, idx)
+        return '{}/loc_{}.npy'.format(self.sig_location, idx)
+
+    def guess_location(self):
+        sig = self.location_sig()
+        min_idx = None
+        min_err = np.inf
+        for shift in range(0, self.num_bins):
+            errors = np.sum((self.signatures - np.roll(sig, shift))**2, axis=1)
+            if np.min(errors) < min_err:
+                min_idx = np.argmin(errors)
+                min_err = np.min(errors)
+        return min_idx
